c86593384989e3b3b6f54a266008b71995eabedc
-v <benjamin@Benjamins-MacBook-Air.local>
2016-04-05 12:27:06 -0700

    Insertion sort load and store code finished

--- assignments/PA8/insertion_sort_proc.asm
+++ assignments/PA8/insertion_sort_proc.asm
@@ -12,11 +12,20 @@
 #-------------------------------------------
 insertion_sort:
 	# Caller RTE store (TBD)
-
+	addi	$sp, $sp, -20
+	sw	$fp, 0($sp)
+	sw	$a0, 4($sp)
+	sw	$a1, 8($sp)
+	sw	$s0, 12($sp)
+	addi	$fp, $sp, 20
 	# Implement insertion sort (TBD)
 	
 insertion_sort_end:
 	# Caller RTE restore (TBD)
-	
+	lw	$fp, 0($sp)
+	lw	$a0, 4($sp)
+	lw	$a1, 8($sp)
+	lw	$s0, 12($sp)
+	addi	$sp, $sp, 20
 	# Return to Caller
 	jr	$ra
